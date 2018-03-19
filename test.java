d1ff8c5dcd38df4adc3c9155024a27f0a9eff5e4
luyanda <luyandambodla@gmail.com>
2018-03-19 04:36:26 +0200

    Second TravisTest

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test extends PojoA{
     public test(String name, int age) {
-        super(name, age);
+        super(name, name);
     }
     public void print(){
         System.out.println("My name is "+getName()+", and I am "+getAge()+".");
