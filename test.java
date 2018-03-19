9b8e2bbb87218ebf47766da76b4712a112a682c1
luyanda <luyandambodla@gmail.com>
2018-03-19 13:19:14 +0200

    Third test

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test extends PojoA{
     public test(String name, int age) {
-        super(name, name);
+        super(name, age);
     }
     public void print(){
         System.out.println("My name is "+getName()+", and I am "+getAge()+".");
